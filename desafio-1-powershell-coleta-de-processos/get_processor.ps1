c44588d7d907bfd0c4e8d221505067d4fbb4509a
Rodrigo da Silva <raswolf23@gmail.com>
2022-08-08 11:36:27 -0300

    Solution with comments

--- desafio-1-powershell-coleta-de-processos/get_processor.ps1
+++ desafio-1-powershell-coleta-de-processos/get_processor.ps1
@@ -1,10 +1,17 @@
-$processes = Get-Counter '\Process(*)\% Processor Time'
-$cpu_cores = (Get-WMIObject Win32_ComputerSystem).NumberOfLogicalProcessors
-$tenpercent = ($cpu_cores*100)*0.10
-foreach ($process in $processes.countersamples) {
-    # $process.InstanceName
+$processes = Get-Counter '\Process(*)\% Processor Time' # Get all currently runnig processes and their respective properties 
+
+$cpu_cores = (Get-WMIObject Win32_ComputerSystem).NumberOfLogicalProcessors # Get the number of logical processors
+
+$tenpercent = ($cpu_cores*100)*0.10 # Stores the 10% proportional value based on the number of processors
+
+foreach ($process in $processes.countersamples) { # Execution loop for each running process
+    
+    #If current process usage is greater than proportional for all processors, ignoring default processes "idle" and "_total"
     if($process.cookedvalue -gt $tenpercent -and $process.InstanceName -notLike 'idle' -and $process.InstanceName -notLike '_total'){
-       write-host "Nome do processo: $($process.InstanceName)"
-       write-host "Uso de CPU: $($process.cookedvalue / $cpu_cores)"
+       
+       write-host "Nome do processo: $($process.InstanceName)" # Show on console the process name
+
+       write-host "Uso de CPU: $($process.cookedvalue / $cpu_cores)" # Show on console the process value
+
     }
 }
